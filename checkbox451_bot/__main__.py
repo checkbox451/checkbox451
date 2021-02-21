336e7a230ef05ac2fea77d2c519e5caa99f124a6
Mystic-Mirage <mm@m10e.net>
2021-02-21 23:20:48 +0200

    Remove __main__ check

--- checkbox451_bot/__main__.py
+++ checkbox451_bot/__main__.py
@@ -2,11 +2,10 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
-if __name__ == "__main__":
-    from checkbox451_bot import auth, goods, handlers, kbd
+from checkbox451_bot import auth, goods, handlers, kbd
 
-    auth.init()
-    goods.init()
-    kbd.init()
+auth.init()
+goods.init()
+kbd.init()
 
-    handlers.start_polling()
+handlers.start_polling()
