4c47b510ddf279fa00d17d3b6f11f31fe8ac354a
Mystic-Mirage <mm@m10e.net>
2021-05-27 22:08:04 +0300

    Improve test_escpos.py

--- tests/test_escpos.py
+++ tests/test_escpos.py
@@ -1,9 +1,18 @@
+from escpos.constants import CODEPAGE_CHANGE
 from escpos.printer import File
 
 
 def test_escpos(tmp_path):
     dst_file = tmp_path / "output.bin"
-    printer = File(str(dst_file))
-    printer.text("абв")
+    printer = File(str(dst_file), profile="POS-5890")
+    printer.text("абвїґ")
 
-    assert dst_file.read_bytes() == b"\x1bt\x11\xa0\xa1\xa2"
+    expected = (
+        CODEPAGE_CHANGE
+        + bytes([59])  # CP866
+        + "абвї".encode("cp866")
+        + CODEPAGE_CHANGE
+        + bytes([73])  # CP1251
+        + "ґ".encode("cp1251")
+    )
+    assert dst_file.read_bytes() == expected
