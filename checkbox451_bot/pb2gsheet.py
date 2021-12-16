21e2396fa5a9188879df6de62355a54171412a96
Mystic-Mirage <mm@m10e.net>
2021-12-16 22:57:46 +0200

    Add new parse mode for a payment description to extract a sender

--- checkbox451_bot/pb2gsheet.py
+++ checkbox451_bot/pb2gsheet.py
@@ -16,7 +16,9 @@ from checkbox451_bot import __product__, auth, gsheet
 from checkbox451_bot.handlers import bot, helpers
 
 URL = "https://acp.privatbank.ua/api/statements/transactions"
-sender_pat = re.compile(r"^.+,\s*(\S+\s+\S+(?:\s+)?\S+)\s*$")
+sender_pat = re.compile(
+    r"^(?:.+(?:,\s*|Переказ\s+вiд\s+)(\S+\s+\S+(?:\s+\S+)?))\s*$"
+)
 
 accounts = [
     acc for acc in os.environ.get("PRIVAT24_ACCOUNTS", "").split(",") if acc
